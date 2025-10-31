8be71da0e668c3475f5b6c6bff6f827e6aeb3765
5Stygian <rlcrafistohard@gmail.com>
2025-10-31 15:42:42 -0700

    start on syntax highlighting

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
-const vpWidth  = screen.width;
-const vpHeight = screen.height;
-
-const entirePage = document.getElementByTagName("html");
-
-entirePage.style.width  = vpWidth;
-entirePage.style.height = vpHeight;
+// syntax highlighting
+const highlight = document.getElementById("highlight");
+let highlightContent = highlight.innerHTML;
+const keywords1 = ['self', 'None', 'True', 'False'];
+let newText;
+for (let i = 0; i < keywords1.length; i++) {
+    newText = highlightContent.replace(new RegExp(keywords1[i], 'g'), `<span style="color:oklch(0.5009 0.2245 351.69);">${keywords1[i]}</span>`);
+    console.log(newText);
+}
+highlightContent = newText;

a607d0a257fd4d4d347146ebd4fca1497bae7b8f
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-10-31 10:25:43 -0700

    hoping and praying that this works

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,6 @@ const vpWidth  = screen.width;
 const vpHeight = screen.height;
 
 const entirePage = document.getElementByTagName("html");
+
+entirePage.style.width  = vpWidth;
+entirePage.style.height = vpHeight;

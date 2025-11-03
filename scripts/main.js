f7472703dcaf572797ba31221dacbdf252ecc7cf
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-11-03 11:12:54 -0800

    start on collapsibles

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
+const collapsible = document.getElementById("collapsible");
+const collapsibleInner = collapsible.innerHTML;
 
+collapsible.addEventListener("click", function() {
+  collapsibleInner.style.display = none;
+});

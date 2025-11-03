6f1f8ecacfb66b58a0cbdf6bf898727503b2cade
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-11-03 12:22:38 -0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 // this code is from W3
 const coll = document.getElementsByClassName("collapsible");
-coll.innerHTML = "Show";
 
 for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
@@ -8,10 +7,8 @@ for (let i = 0; i < coll.length; i++) {
     let content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
-      coll.innerHTML = "Hide";
     } else {
       content.style.display = "block";
-      coll.innerHTML = "Show";
     }
   });
 }
